6274d09c78b457e597bbdbad18f8bad12496195d
Giorgio Comai <giorgiocomai@gmail.com>
2019-03-16 13:05:11 +0100

    introducing only_cached parameter

--- R/find_gender.R
+++ R/find_gender.R
@@ -4,6 +4,7 @@
 #' @param language Two letters language codes to be passed to WikiData
 #' @param cache Logical, defaults to TRUE. IF TRUE, it stores data on the search string and the resulting Wikidata object. Data are stored inside a `wikidata` folder (if it does not exist, it is automatically created).
 #' @param wait Numeric, defaults to 0.1. Waiting time between queries, relevant when making a large number of queries. 
+#' @param only_cached Logical, defaults to FALSE. If TRUE, it only uses locally cached information.
 #' @param exclude_without_uppercase Logical, degaults to TRUE. If the search string does not include any upper case letter, then assume it's not a person.
 #' @return A data.frame (a tibble) with one row and four columns: Query, Gender, Description, and WikidataID
 #' @examples
@@ -14,7 +15,12 @@
 #' 
 
 
-FindGender <- function(search, language = "en", cache = TRUE, wait = 0.1, exclude_without_uppercase = TRUE) {
+FindGender <- function(search,
+                       language = "en",
+                       cache = TRUE,
+                       wait = 0.1,
+                       only_cached = FALSE,
+                       exclude_without_uppercase = TRUE) {
   # If no capital letters, just say it's not a person
   if (exclude_without_uppercase==TRUE&stringr::str_detect(string = search, pattern = stringr::regex(pattern = "\\p{Lu}"))==FALSE) {
     return(tibble::tibble(Query = search, Gender = as.character(NA), Description = NA, WikidataID = NA)) 
@@ -29,6 +35,9 @@ FindGender <- function(search, language = "en", cache = TRUE, wait = 0.1, exclud
   # search term
   search_response_location <- file.path("wikidata", "search", language, paste0(gsub(pattern = "/", replacement = "_", x = search), ".rds"))
   if (file.exists(search_response_location)==FALSE) {
+    if (only_cached==TRUE) {
+      return(NULL)
+    }
     search_response <- tryCatch(WikidataR::find_item(search_term = search, language = language, limit = 3), error = function(e) warning(e))
     if (is.null(search_response)) (return(NULL))
     saveRDS(object = search_response, file = search_response_location)
