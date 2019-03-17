ea9e3606fedd44d5752334e87e1e6a1e2a02cbc7
Giorgio Comai <giorgiocomai@gmail.com>
2019-03-17 15:01:25 +0100

    fix subfolder for selecting cities

--- R/app_server.R
+++ R/app_server.R
@@ -1,7 +1,7 @@
 #' @import shiny
 app_server <- function(input, output,session) {
   # List the first level callModules here
-  callModule(module = mod_select_dataset, id =  "select_dataset_ui_1", dataset_folder_name = "data")
+  callModule(module = mod_select_dataset, id =  "select_dataset_ui_1", dataset_folder_name = "data/cities")
   
   #output$current_folder <- shiny::renderText(expr = getwd())
   
